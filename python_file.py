4a885a592cc1fc200fa20fc05786dd15d8cac16d
Matthew Wright <matthewbwright@gmail.com>
2019-04-29 08:27:20 -0700

    adds .csv file

--- python_file.py
+++ python_file.py
@@ -3,3 +3,9 @@ import csv
 
 # create a list of users whose passwords have been compromised
 compromised_users = []
+
+# open the file, stored in password_file
+with open('passwords.csv') as password_file:
+    password_csv = csv.DictReader(password_file)
+    for row in password_csv:
+        password_row = print(password_row['Username'])
